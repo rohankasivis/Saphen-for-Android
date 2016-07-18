42a185ff496dffd4950bf138c6811e38199822c9
Rohan Kasiviswanathan <rohan.kasivis@gmail.com>
2016-07-17 20:35:03 -0500

    changes made

--- app/src/main/java/saphen_for_android/saphen_for_android/HomePage.java
+++ app/src/main/java/saphen_for_android/saphen_for_android/HomePage.java
@@ -3,10 +3,6 @@ package saphen_for_android.saphen_for_android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class HomePage extends Activity
--- app/src/main/java/saphen_for_android/saphen_for_android/MainActivity.java
+++ app/src/main/java/saphen_for_android/saphen_for_android/MainActivity.java
@@ -6,9 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -58,9 +58,14 @@ public class MainActivity extends AppCompatActivity {
             Intent curr = new Intent(MainActivity.this, HomePage.class);
             startActivity(curr);
         }
-        else if(view.getId() == R.id.the_login)
+        else if(view.getId() == R.id.login_button)
         {
             Intent intent = new Intent(MainActivity.this, StatisticsActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.create_button)
+        {
+            Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
             startActivity(intent);
         }
     }
